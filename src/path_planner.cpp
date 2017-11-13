54b2989fff9fe7683280c72bc85aeb1ebb753fb8
Andy Knight <andy.knight@guidance.eu.com>
2017-11-13 20:57:41 +0000

    Refactoring

--- src/path_planner.cpp
+++ src/path_planner.cpp
@@ -3,24 +3,26 @@
 
 #define MPS_TO_MPH 2.23694  // Conversion between m/s and mph
 #define MPH_TO_MPS (1 / MPS_TO_MPH)
-
-#define TARGET_SPEED 49.5 // mph
-#define MAX_ACCELERATION 5.0 // m/s^2
 #define DT 0.02 // Time step (s)
+
+#define LEFT_LANE 0
+#define MIDDLE_LANE 1
+#define RIGHT_LANE 2
+
+#define TARGET_TOP_SPEED 49.5 // mph
+#define MAX_ACCELERATION 5.0 // m/s^2
 #define LANE_WIDTH 4.0 // m
 #define CAR_WIDTH 3.0 // m
 #define MIN_FOLLOW_DISTANCE 25.0 // m
 #define MIN_LANE_CHANGE_GAP 25.0 // m
 #define NUM_TRAJECTORY_POINTS 50
 
-#define LEFT_LANE 0
-#define MIDDLE_LANE 1
-#define RIGHT_LANE 2
-
 PathPlanner::PathPlanner(const Tools & tools)
     : tools(tools)
 {
     // Start stationary in the middle lane
+    state = KeepLane;
+    target_vel = TARGET_TOP_SPEED;
     ref_vel = 0;
     ref_lane = MIDDLE_LANE;
 }
@@ -29,68 +31,78 @@ PathPlanner::~PathPlanner()
 {
 }
 
-void PathPlanner::UpdateTrajectory(MeasurementData & data, vector<double> & next_x_vals, vector<double> & next_y_vals)
+void PathPlanner::UpdateTrajectory(MeasurementData & latest_data, vector<double> & next_x_vals, vector<double> & next_y_vals)
 {
-    int prev_size = data.previous_path_x.size();
+    // Store latest measurement data
+    data = latest_data;
 
     // Plan trajectory from the end of the current trajectory
-    if (prev_size > 0)
+    if (data.prev_size() > 0)
     {
         data.car_s = data.end_path_s;
     }
 
+    // Behaviour Planning
+    DetermineNextAction();
+
+    // Trajectory generation
+    CalculateTrajectory(next_x_vals, next_y_vals);
+}
+
+void PathPlanner::DetermineNextAction()
+{
     // Check if we are too close to any cars (in our lane)
-    double max_speed = CalculateLaneSpeed(ref_lane, data, prev_size);
+    target_vel = CalculateLaneSpeed(ref_lane);
 
     // See if we need to change lane
-    if (max_speed < TARGET_SPEED)
+    if (target_vel < TARGET_TOP_SPEED)
     {
         // Check options
         if (ref_lane == LEFT_LANE || ref_lane == RIGHT_LANE)
         {
-            bool can_move_into_middle_lane = CanMoveIntoLane(MIDDLE_LANE, data, prev_size);
-            double middle_lane_speed = CalculateLaneSpeed(MIDDLE_LANE, data, prev_size);
-            if (can_move_into_middle_lane && middle_lane_speed > max_speed)
+            bool can_move_into_middle_lane = CanMoveIntoLane(MIDDLE_LANE);
+            double middle_lane_speed = CalculateLaneSpeed(MIDDLE_LANE);
+            if (can_move_into_middle_lane && middle_lane_speed > target_vel)
             {
                 ref_lane = MIDDLE_LANE;
-                max_speed = middle_lane_speed;
-            } 
+                target_vel = middle_lane_speed;
+            }
         }
         else
         {
             // Can move into either left or right lane
-            bool can_move_into_left_lane = CanMoveIntoLane(LEFT_LANE, data, prev_size);
-            double left_lane_speed = CalculateLaneSpeed(LEFT_LANE, data, prev_size);
-            bool can_move_into_right_lane = CanMoveIntoLane(RIGHT_LANE, data, prev_size);
-            double right_lane_speed = CalculateLaneSpeed(RIGHT_LANE, data, prev_size);
+            bool can_move_into_left_lane = CanMoveIntoLane(LEFT_LANE);
+            double left_lane_speed = CalculateLaneSpeed(LEFT_LANE);
+            bool can_move_into_right_lane = CanMoveIntoLane(RIGHT_LANE);
+            double right_lane_speed = CalculateLaneSpeed(RIGHT_LANE);
             if (can_move_into_left_lane && can_move_into_right_lane)
             {
                 if (left_lane_speed > right_lane_speed)
                 {
-                    if (left_lane_speed > max_speed)
+                    if (left_lane_speed > target_vel)
                     {
                         ref_lane = LEFT_LANE;
-                        max_speed = left_lane_speed;
+                        target_vel = left_lane_speed;
                     }
                 }
                 else
                 {
-                    if (right_lane_speed > max_speed)
+                    if (right_lane_speed > target_vel)
                     {
                         ref_lane = RIGHT_LANE;
-                        max_speed = right_lane_speed;
+                        target_vel = right_lane_speed;
                     }
                 }
             }
-            else if (can_move_into_left_lane && left_lane_speed > max_speed)
+            else if (can_move_into_left_lane && left_lane_speed > target_vel)
             {
                 ref_lane = LEFT_LANE;
-                max_speed = left_lane_speed;
+                target_vel = left_lane_speed;
             }
-            else if (can_move_into_right_lane && right_lane_speed > max_speed)
+            else if (can_move_into_right_lane && right_lane_speed > target_vel)
             {
                 ref_lane = RIGHT_LANE;
-                max_speed = right_lane_speed;
+                target_vel = right_lane_speed;
             }
             else
             {
@@ -98,9 +110,67 @@ void PathPlanner::UpdateTrajectory(MeasurementData & data, vector<double> & next
             }
         }
     }
+}
 
-    
+double PathPlanner::CalculateLaneSpeed(int lane)
+{
+    double lane_speed = TARGET_TOP_SPEED;
+    for (int i = 0; i < data.sensor_fusion.size(); i++)
+    {
+        // Check if this car is in our lane
+        float d = data.sensor_fusion[i][6];
+        if ((LANE_WIDTH / 2 + LANE_WIDTH * lane - CAR_WIDTH / 2) < d && d < (LANE_WIDTH / 2 + LANE_WIDTH * lane + CAR_WIDTH / 2))
+        {
+            // Calculate speed of other car
+            double vx = data.sensor_fusion[i][3];
+            double vy = data.sensor_fusion[i][4];
+            double check_speed = sqrt(vx*vx + vy*vy);
 
+            // Predict position of car at point in time that we are planning from (in the future)
+            double check_car_s = data.sensor_fusion[i][5];
+            check_car_s += ((double)data.prev_size() * DT * check_speed);
+
+            // Check if we will be too close to the car
+            if ((check_car_s > data.car_s) && ((check_car_s - data.car_s) < MIN_FOLLOW_DISTANCE))
+            {
+                // Set flag to say that we need to slow down
+                lane_speed = fmin(lane_speed, check_speed);
+            }
+        }
+    }
+    return lane_speed;
+}
+
+bool PathPlanner::CanMoveIntoLane(int lane)
+{
+    for (int i = 0; i < data.sensor_fusion.size(); i++)
+    {
+        // Check if this car is in our lane
+        float d = data.sensor_fusion[i][6];
+        if ((LANE_WIDTH / 2 + LANE_WIDTH * lane - CAR_WIDTH / 2) < d && d < (LANE_WIDTH / 2 + LANE_WIDTH * lane + CAR_WIDTH / 2))
+        {
+            // Calculate speed of other car
+            double vx = data.sensor_fusion[i][3];
+            double vy = data.sensor_fusion[i][4];
+            double check_speed = sqrt(vx*vx + vy*vy);
+
+            // Predict position of car at point in time that we are planning from (in the future)
+            double check_car_s = data.sensor_fusion[i][5];
+            check_car_s += ((double)data.prev_size() * DT * check_speed);
+
+            // Check if we will be too close to the car
+            if (((check_car_s - data.car_s) > (MIN_FOLLOW_DISTANCE - MIN_LANE_CHANGE_GAP)) && ((check_car_s - data.car_s) < MIN_FOLLOW_DISTANCE))
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+void PathPlanner::CalculateTrajectory(vector<double> & next_x_vals, vector<double> & next_y_vals)
+{
     // Define sparse waypoints (30m apart) used to construct spline trajectory
     vector<double> ptsx, ptsy;
 
@@ -108,7 +178,7 @@ void PathPlanner::UpdateTrajectory(MeasurementData & data, vector<double> & next
     double ref_x, ref_y, ref_yaw;
 
     // Check if we have a previous trajectory to work from
-    if (prev_size < 2)
+    if (data.prev_size() < 2)
     {
         // Not enough points to work from so use current car state to define first two points for spline
         ref_x = data.car_x;
@@ -128,11 +198,11 @@ void PathPlanner::UpdateTrajectory(MeasurementData & data, vector<double> & next
     else
     {
         // Use existing trajectory to define first two points for spline
-        ref_x = data.previous_path_x[prev_size - 1];
-        ref_y = data.previous_path_y[prev_size - 1];
+        ref_x = data.previous_path_x[data.prev_size() - 1];
+        ref_y = data.previous_path_y[data.prev_size() - 1];
 
-        double ref_x_prev = data.previous_path_x[prev_size - 2];
-        double ref_y_prev = data.previous_path_y[prev_size - 2];
+        double ref_x_prev = data.previous_path_x[data.prev_size() - 2];
+        double ref_y_prev = data.previous_path_y[data.prev_size() - 2];
 
         // Calculate reference yaw based on last two points in trajectory
         ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
@@ -174,7 +244,7 @@ void PathPlanner::UpdateTrajectory(MeasurementData & data, vector<double> & next
     s.set_points(ptsx, ptsy);
 
     // Start from existing trajectory
-    for (int i = 0; i < prev_size; i++)
+    for (int i = 0; i < data.prev_size(); i++)
     {
         next_x_vals.push_back(data.previous_path_x[i]);
         next_y_vals.push_back(data.previous_path_y[i]);
@@ -182,14 +252,14 @@ void PathPlanner::UpdateTrajectory(MeasurementData & data, vector<double> & next
 
     // Add new trajectory points using spline
     double x_addon = 0;
-    for (int i = 0; i < NUM_TRAJECTORY_POINTS - prev_size; i++)
+    for (int i = 0; i < NUM_TRAJECTORY_POINTS - data.prev_size(); i++)
     {
         // Choose acceleration based on whether we are too close to a car in front
-        if (ref_vel > max_speed)
+        if (ref_vel > target_vel)
         {
             ref_vel -= MAX_ACCELERATION * DT * MPS_TO_MPH;
         }
-        else if (ref_vel < max_speed)
+        else if (ref_vel < target_vel)
         {
             ref_vel += MAX_ACCELERATION * DT * MPS_TO_MPH;
         }
@@ -207,61 +277,4 @@ void PathPlanner::UpdateTrajectory(MeasurementData & data, vector<double> & next
         next_x_vals.push_back(x_point_world);
         next_y_vals.push_back(y_point_world);
     }
-}
-
-double PathPlanner::CalculateLaneSpeed(int lane, const MeasurementData & data, int prev_size)
-{
-    double lane_speed = TARGET_SPEED;
-    for (int i = 0; i < data.sensor_fusion.size(); i++)
-    {
-        // Check if this car is in our lane
-        float d = data.sensor_fusion[i][6];
-        if ((LANE_WIDTH / 2 + LANE_WIDTH * lane - CAR_WIDTH / 2) < d && d < (LANE_WIDTH / 2 + LANE_WIDTH * lane + CAR_WIDTH / 2))
-        {
-            // Calculate speed of other car
-            double vx = data.sensor_fusion[i][3];
-            double vy = data.sensor_fusion[i][4];
-            double check_speed = sqrt(vx*vx + vy*vy);
-
-            // Predict position of car at point in time that we are planning from (in the future)
-            double check_car_s = data.sensor_fusion[i][5];
-            check_car_s += ((double)prev_size * DT * check_speed);
-
-            // Check if we will be too close to the car
-            if ((check_car_s > data.car_s) && ((check_car_s - data.car_s) < MIN_FOLLOW_DISTANCE))
-            {
-                // Set flag to say that we need to slow down
-                lane_speed = fmin(lane_speed, check_speed);
-            }
-        }
-    }
-    return lane_speed;
-}
-
-bool PathPlanner::CanMoveIntoLane(int lane, const MeasurementData & data, int prev_size)
-{
-    for (int i = 0; i < data.sensor_fusion.size(); i++)
-    {
-        // Check if this car is in our lane
-        float d = data.sensor_fusion[i][6];
-        if ((LANE_WIDTH / 2 + LANE_WIDTH * lane - CAR_WIDTH / 2) < d && d < (LANE_WIDTH / 2 + LANE_WIDTH * lane + CAR_WIDTH / 2))
-        {
-            // Calculate speed of other car
-            double vx = data.sensor_fusion[i][3];
-            double vy = data.sensor_fusion[i][4];
-            double check_speed = sqrt(vx*vx + vy*vy);
-
-            // Predict position of car at point in time that we are planning from (in the future)
-            double check_car_s = data.sensor_fusion[i][5];
-            check_car_s += ((double)prev_size * DT * check_speed);
-
-            // Check if we will be too close to the car
-            if (((check_car_s - data.car_s) > (MIN_FOLLOW_DISTANCE - MIN_LANE_CHANGE_GAP)) && ((check_car_s - data.car_s) < MIN_FOLLOW_DISTANCE))
-            {
-                return false;
-            }
-        }
-    }
-
-    return true;
 }
--- src/path_planner.h
+++ src/path_planner.h
@@ -17,12 +17,23 @@ struct MeasurementData
     nlohmann::basic_json<>::value_type previous_path_x;
     nlohmann::basic_json<>::value_type previous_path_y;
     
+    int prev_size() const { return previous_path_x.size(); }
+
     // Previous path's end s and d values 
     double end_path_s;
     double end_path_d;
 
     // Sensor Fusion Data, a list of all other cars on the same side of the road.
     nlohmann::basic_json<>::value_type sensor_fusion;
+};
+
+enum State
+{
+    KeepLane,
+    LaneChangeLeft,
+    LaneChangeRight,
+    PrepareLaneChangeLeft,
+    PrepareLaneChangeRIght
 };
 
 class PathPlanner
@@ -33,12 +44,18 @@ public:
 
 public:
     void UpdateTrajectory(MeasurementData & data, vector<double> & next_x_vals, vector<double> & next_y_vals);
-    double CalculateLaneSpeed(int lane, const MeasurementData & data, int prev_size);
-    bool CanMoveIntoLane(int lane, const MeasurementData & data, int prev_size);
+    void DetermineNextAction();
+    double CalculateLaneSpeed(int lane);
+    bool CanMoveIntoLane(int lane);
+    void CalculateTrajectory(vector<double> & next_x_vals, vector<double> & next_y_vals);
 
 private:
     const Tools & tools;
 
+    State state;
+    MeasurementData data;
+
+    double target_vel;
     int ref_lane;
     double ref_vel;
 };
